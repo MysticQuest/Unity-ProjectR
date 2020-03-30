a1b476d4fa2640b1bb387110adaaee2ab18d719f
Mystic Quest <l1skou@gmail.com>
2020-03-30 23:17:07 +0300

    misc

---
+++ Assets/Scripts/PlayerInputMouse.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Utility;
+
+public class PlayerInputMouse : MonoBehaviour
+{
+    private MoveToPositionDirect moveTo;
+
+    private void Awake()
+    {
+        moveTo = GetComponent<MoveToPositionDirect>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            moveTo.SetMovePosition(Utilities.GetMousePosition());
+        }
+    }
+}
