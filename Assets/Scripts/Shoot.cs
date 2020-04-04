71401427ae64dba44bd569f12b154ecd7c49625a
Mystic Quest <l1skou@gmail.com>
2020-04-04 23:46:09 +0300

    bullet setup pt1

---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+
+    [SerializeField] private Transform pfBullet;
+
+    private void Awake()
+    {
+
+    }
+
+    private void shoot()
+    {
+
+    }
+}
