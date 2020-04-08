97c17073450b5c51049680814e2d4fb09cf17f1a
Mystic Quest <l1skou@gmail.com>
2020-04-08 07:55:35 +0300

    mesh system pt2, custom vectors/geometry

--- Assets/Scripts/MeshParticleSystem.cs
+++ Assets/Scripts/MeshParticleSystem.cs
@@ -1,12 +1,34 @@
-﻿using System.Collections;
+﻿using System;
+using System.Linq;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using CodeMonkey.Utils;
 
 public class MeshParticleSystem : MonoBehaviour
 {
     private const int MAX_QUAD_AMOUNT = 15000;
 
+    //set in editor using texture pixel values
+    [System.Serializable]
+    public struct ParticleUVPixels
+    {
+        public Vector2Int uv00Pixels;
+        public Vector2Int uv11Pixels;
+    }
+
+    //normalized texture UV coordinates
+    private struct UVCoords
+    {
+        public Vector2 uv00;
+        public Vector2 uv11;
+    }
+
     [SerializeField] private Shoot playerShoot;
+
+    [SerializeField] private ParticleUVPixels[] particleUVPixelsArray;
+    private UVCoords uvCoordsArray;
+
     private Mesh mesh;
 
     private Vector3[] vertices;
@@ -30,17 +52,57 @@ public class MeshParticleSystem : MonoBehaviour
         mesh.triangles = triangles;
 
         GetComponent<MeshFilter>().mesh = mesh;
+
+        //Setup internal UV normalized array
+        Material material = GetComponent<MeshRenderer>().material;
+        Texture mainTexture = material.mainTexture;
+        int textureWidth = mainTexture.width;
+        int textureHeight = mainTexture.height;
+
+        List<UVCoords> uvCoordsList = new List<UVCoords>();
+        foreach (ParticleUVPixels particleUVPixels in particleUVPixelsArray)
+        {
+            UVCoords uvCoords = new UVCoords
+            {
+                uv00 = new Vector2((float)particleUVPixels.uv00Pixels.x / textureWidth, (float)particleUVPixels.uv00Pixels.y / textureHeight),
+                uv11 = new Vector2((float)particleUVPixels.uv11Pixels.x / textureWidth, (float)particleUVPixels.uv11Pixels.y / textureHeight)
+            };
+            uvCoordsList.Add(uvCoords);
+        }
+        uvCoordsArray = uvCoordsList.ToArray();
     }
 
     private void PlayerShoot_OnShoot(object sender, Shoot.OnShootEventArgs e)
     {
-        AddQuad(e.gunEndPointPosition);
+        Vector3 quadPosition = e.gunEndPointPosition;
+        Vector3 quadSize = new Vector3(.5f, 1f);
+        float rotation = 0f;
+
+        int spawnedQuadIndex = AddQuad(quadPosition, rotation, quadSize, true);
+
+        FunctionUpdater.Create(() =>
+        {
+            quadPosition += new Vector3(1, 1) * Time.deltaTime;
+            quadSize += new Vector3(1, 1) * Time.deltaTime;
+            rotation += 360f * Time.deltaTime;
+            UpdateQuad(spawnedQuadIndex, quadPosition, rotation, quadSize, true);
+        });
     }
 
-    private void AddQuad(Vector3 position)
+    private int AddQuad(Vector3 position, float rotation, Vector3 quadSize, bool skewed)
     {
-        if (quadIndex >= MAX_QUAD_AMOUNT) return; //mesh full
+        if (quadIndex >= MAX_QUAD_AMOUNT) return 0; //mesh full
 
+        UpdateQuad(quadIndex, position, rotation, quadSize, skewed);
+
+        int spawnedQuadIndex = quadIndex;
+        quadIndex++;
+
+        return spawnedQuadIndex;
+    }
+
+    public void UpdateQuad(int quadIndex, Vector3 position, float rotation, Vector3 quadSize, bool skewed)
+    {
         //relocate vertices
         int vIndex = quadIndex * 4;
         int vIndex0 = vIndex;
@@ -48,12 +110,20 @@ public class MeshParticleSystem : MonoBehaviour
         int vIndex2 = vIndex + 2;
         int vIndex3 = vIndex + 3;
 
-        Vector3 quadSize = new Vector3(1, 1);
-        float rotation = 0f;
-        vertices[vIndex0] = position + Quaternion.Euler(0, 0, rotation - 180) * quadSize;
-        vertices[vIndex1] = position + Quaternion.Euler(0, 0, rotation - 270) * quadSize;
-        vertices[vIndex2] = position + Quaternion.Euler(0, 0, rotation - 0) * quadSize;
-        vertices[vIndex3] = position + Quaternion.Euler(0, 0, rotation - 90) * quadSize;
+        if (skewed)
+        {
+            vertices[vIndex0] = position + Quaternion.Euler(0, 0, rotation) * new Vector3(-quadSize.x, -quadSize.y);
+            vertices[vIndex1] = position + Quaternion.Euler(0, 0, rotation) * new Vector3(-quadSize.x, +quadSize.y);
+            vertices[vIndex2] = position + Quaternion.Euler(0, 0, rotation) * new Vector3(+quadSize.x, +quadSize.y);
+            vertices[vIndex3] = position + Quaternion.Euler(0, 0, rotation) * new Vector3(+quadSize.x, -quadSize.y);
+        }
+        else
+        {
+            vertices[vIndex0] = position + Quaternion.Euler(0, 0, rotation - 180) * quadSize;
+            vertices[vIndex1] = position + Quaternion.Euler(0, 0, rotation - 270) * quadSize;
+            vertices[vIndex2] = position + Quaternion.Euler(0, 0, rotation - 0) * quadSize;
+            vertices[vIndex3] = position + Quaternion.Euler(0, 0, rotation - 90) * quadSize;
+        }
 
         //UV
         uv[vIndex0] = new Vector2(0, 0);
@@ -72,10 +142,10 @@ public class MeshParticleSystem : MonoBehaviour
         triangles[tIndex + 4] = vIndex2;
         triangles[tIndex + 5] = vIndex3;
 
-        quadIndex++;
-
         mesh.vertices = vertices;
         mesh.uv = uv;
         mesh.triangles = triangles;
+
+        mesh.RecalculateBounds();
     }
 }
